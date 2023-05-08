e362032e4d0697464f7a2ca3075a92e351216fac
Michal Štecher <m.stecher@cleverbee.com>
2023-05-08 19:24:20 +0200

    Project setup: tailwind, prettier, husky

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [require('prettier-plugin-tailwindcss')],
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    pluginSearchDirs: false,
+}
