866248ca431c3e46ffa874947af87d942f6f7d51
Michal Štecher <m.stecher@cleverbee.com>
2023-05-15 19:26:06 +0200

    feat: move typing-area into typing page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { TypingPageComponent } from './features/typing/pages/typing-page/typing-page.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: 'typing', component: TypingPageComponent },
+  // other routes...
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
