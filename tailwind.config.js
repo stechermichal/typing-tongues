958eb61985966ae443e4f4d9f6b79bdda2be3621
Michal Štecher <m.stecher@cleverbee.com>
2023-05-16 21:35:01 +0200

    style: general colorscheme to follow in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{html,ts}'],
+  darkMode: false,
   content: [],
   theme: {
+    extend: {
+      colors: {
+        background: colors.gray[900],
+        backgroundSecondary: colors.gray[800],
+        backgroundTertiary: colors.gray[700],
+        primary: colors.green[500],
+        secondary: colors.gray[600],
+        tertiary: colors.blue[600],
+        quaternary: colors.purple[600],
+        accent: colors.purple[400],
+        highlight: colors.blue[400],
+        hover: colors.purple[300],
+        error: colors.red[500],
+        info: colors.cyan[400],
+        warning: colors.yellow[400],
+        success: colors.green[400],
+        muted: colors.gray[500],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
