231d770aff537d951e6005c6a73bcc638f11a665
Michal Štecher <m.stecher@cleverbee.com>
2023-05-29 12:04:39 +0200

    docs: add overly verbose and descriptive comments related to theme selection to make it easier for anyone to create a new theme

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -9,7 +9,9 @@ import { ThemeService } from '../services/theme.service'
 export class HeaderComponent {
   constructor(private themeService: ThemeService) {}
 
+  // Called when user selects a new theme from the dropdown
   switchTheme(theme: string) {
+    // Pass the new theme value to the ThemeService
     this.themeService.theme = theme
   }
 }
--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -5,26 +5,39 @@ import { BehaviorSubject } from 'rxjs'
   providedIn: 'root',
 })
 export class ThemeService {
+  // Renderer for manipulating classes on the DOM elements
   private renderer: Renderer2
-  // initialize with dark as BehaviorSubject's initial value
+
+  // BehaviorSubject to keep track of current theme, initializes with 'dark' as the default value
   private readonly _theme = new BehaviorSubject<string>('dark')
+
+  // Observable to emit changes in theme for components to subscribe to
   readonly theme$ = this._theme.asObservable()
 
   constructor(rendererFactory: RendererFactory2) {
+    // Create an instance of Renderer
     this.renderer = rendererFactory.createRenderer(null, null)
-    // add the dark theme to the body by default
+
+    // Add the dark theme to the body element by default when the service is created
     this.renderer.addClass(document.body, `theme-${this.theme}`)
   }
 
   get theme(): string {
+    // return the current value of _theme
     return this._theme.getValue()
   }
 
   set theme(val: string) {
-    // when settings a new theme, remove the previous one first
+    // Get the previous theme before changing to new one
     const previousTheme = this.theme
+
+    // Remove the class of the previous theme from body
     this.renderer.removeClass(document.body, `theme-${previousTheme}`)
+
+    // Add the class of the new theme to body
     this.renderer.addClass(document.body, `theme-${val}`)
+
+    // Emit the new theme value through the BehaviorSubject
     this._theme.next(val)
   }
 }
