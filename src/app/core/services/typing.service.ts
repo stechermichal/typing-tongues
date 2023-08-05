e680a835669e93f036560154abeda10fcc9faa8a
Michal Štecher <m.stecher@cleverbee.com>
2023-08-05 14:02:26 +0200

    feat: accuracy stats now preserve the accuracy as if the user hadn't gone back to fix it

--- src/app/core/services/typing.service.ts
+++ src/app/core/services/typing.service.ts
@@ -14,18 +14,35 @@ export class TypingService {
 
   constructor() {}
 
+  // Add a new property to keep track of total keypresses
+  private totalKeyPresses = 0
+  private mistakesMade = 0
+  private maxTypedLength = 0
+
   updateStats(userTyping: string, textToType: string, startTime: number) {
     const elapsedTime = new Date().getTime() - startTime
     const wordsTyped = userTyping.split(' ').length
     const wpm = ((wordsTyped / elapsedTime) * MS_IN_A_MINUTE).toFixed(2)
 
     let correctCharacters = 0
+
     for (let i = 0; i < userTyping.length; i++) {
       if (userTyping[i] === textToType[i]) {
         correctCharacters++
+      } else if (i === this.totalKeyPresses) {
+        // We only count mistakes on new keypresses to avoid overcounting
+        this.mistakesMade++
       }
     }
-    const accuracy = ((correctCharacters / textToType.length) * 100).toFixed(2)
+
+    // Increase the total keypresses after checking for mistakes
+    this.totalKeyPresses = userTyping.length
+
+    // The accuracy now considers mistakes made against total keypresses
+    const accuracy = (
+      (100 * (this.totalKeyPresses - this.mistakesMade)) /
+      this.totalKeyPresses
+    ).toFixed(2)
 
     this.wpmSource.next(Number(wpm))
     this.accuracySource.next(Number(accuracy))
