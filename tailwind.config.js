dc3e84c1046e620219d036022a424a3727f0b5dd
Michal Štecher <m.stecher@cleverbee.com>
2023-08-05 17:51:33 +0200

    feat: add caret to typing-area, animation for the caret into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ const { createThemes } = require('tw-colors')
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: 'blink 1s infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
