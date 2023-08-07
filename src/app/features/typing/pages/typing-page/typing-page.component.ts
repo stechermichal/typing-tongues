2e8500fe6e216f1847ab16c3338b87e2cb7c9135
Michal Štecher <m.stecher@cleverbee.com>
2023-08-07 21:18:05 +0200

    feat: double the typing area on typing-page, blur the unfocused one and hide caret, add enum for languages

--- src/app/features/typing/pages/typing-page/typing-page.component.ts
+++ src/app/features/typing/pages/typing-page/typing-page.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core'
+import { Language } from 'src/app/shared/enums'
 
 @Component({
   selector: 'app-typing-page',
   templateUrl: './typing-page.component.html',
   styleUrls: ['./typing-page.component.css'],
 })
-export class TypingPageComponent {}
+export class TypingPageComponent {
+  Language = Language // To use it in the template
+  isEnglishFocused: boolean | null = null
+
+  setFocus(language: Language) {
+    this.isEnglishFocused = language === Language.English
+  }
+}
