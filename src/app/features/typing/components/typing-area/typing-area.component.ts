3effbf0ae2048b885f9b71233325ff5d06d81f1a
Michal Štecher <m.stecher@cleverbee.com>
2023-08-19 19:59:49 +0200

    refactor: simplification of typing-area

--- src/app/features/typing/components/typing-area/typing-area.component.ts
+++ src/app/features/typing/components/typing-area/typing-area.component.ts
@@ -1,21 +1,16 @@
 import {
   AfterViewInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   ElementRef,
   EventEmitter,
   HostListener,
   Input,
-  OnChanges,
   OnInit,
   Output,
-  SimpleChanges,
   ViewChild,
 } from '@angular/core'
-import { BookService } from 'src/app/core/services/book.service'
 import { TypingService } from 'src/app/core/services/typing.service'
-import { Language, TypingStatus } from 'src/app/shared/enums'
 
 @Component({
   selector: 'app-typing-area',
@@ -24,61 +19,41 @@ import { Language, TypingStatus } from 'src/app/shared/enums'
   changeDetection: ChangeDetectionStrategy.OnPush, // This is more efficient with how often we update here, but has extra requirements
   // with @Input and child components.sIf there is an issue with updating something, this might be the culprit.
 })
-export class TypingAreaComponent implements OnInit, OnChanges, AfterViewInit {
+export class TypingAreaComponent implements OnInit, AfterViewInit {
   @ViewChild('hiddenInput') hiddenInput!: ElementRef
   @Output() focus = new EventEmitter<void>()
   @Output() blur = new EventEmitter<void>()
   @Output() typingCompleted = new EventEmitter<void>()
+
+  private _language: 'nativeTongue' | 'foreignTongue' = 'nativeTongue'
+  @Input()
+  set language(val: 'nativeTongue' | 'foreignTongue') {
+    this._language = val
+    this.resetTyping()
+  }
+  get language(): 'nativeTongue' | 'foreignTongue' {
+    return this._language
+  }
   @Input() autoFocus = true
-  @Input() language: 'nativeTongue' | 'foreignTongue' = 'nativeTongue'
 
   mistakeText = ''
   userTyping: string = ''
   startTime: number = 0
-  textsToType: Record<'nativeTongue' | 'foreignTongue', string> = {
-    nativeTongue: 'the quick brown fox jumps over the lazy dog',
+  textsToType = {
+    nativeTongue:
+      'the quick fox the quick fox the quick fox the quick fox the quick fox the quick fox the quick fox ',
     foreignTongue: 'translated text here',
   }
   textToType: string = this.textsToType[this.language]
   typedText: string = ''
   currentChar = this.textToType[0]
   remainingText = this.textToType.slice(1)
-  correctTyping: string = ''
-  typingStatusEnum = TypingStatus // Expose the enum to the template
   isFocused = false
 
-  constructor(
-    private typingService: TypingService,
-    private bookService: BookService,
-    private cd: ChangeDetectorRef
-  ) {}
+  constructor(private typingService: TypingService) {}
 
   ngOnInit(): void {
-    this.fetchTexts()
     this.resetTyping()
-  }
-
-  fetchTexts(): void {
-    // Define the maximum length of displayed text
-    const maxLength = 1000
-
-    // Fetch English Text
-    this.bookService.getEnglishBook().subscribe((englishText) => {
-      this.textsToType.nativeTongue = englishText.slice(0, maxLength) // slice the text
-      if (this.language === Language.NativeTongue) {
-        this.textToType = this.textsToType.nativeTongue
-        this.resetTyping()
-      }
-    })
-
-    // Fetch German Text
-    this.bookService.getGermanBook().subscribe((germanText) => {
-      this.textsToType.foreignTongue = germanText.slice(0, maxLength) // slice the text
-      if (this.language === Language.ForeignTongue) {
-        this.textToType = this.textsToType.foreignTongue
-        this.resetTyping()
-      }
-    })
   }
 
   ngAfterViewInit(): void {
@@ -91,62 +66,32 @@ export class TypingAreaComponent implements OnInit, OnChanges, AfterViewInit {
     this.userTyping = ''
     this.startTime = 0
     this.mistakeText = ''
+    this.textToType = this.textsToType[this.language]
+    this.typedText = ''
     this.currentChar = this.textToType[0]
     this.remainingText = this.textToType.slice(1)
-    this.correctTyping = ''
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['language'] && changes['language'].currentValue) {
-      this.textToType = this.textsToType[this.language]
-      this.resetTyping()
-    }
-  }
-
-  // Event handler for user typing. This is triggered every time the user types something.
-  @HostListener('input', ['$event'])
-  onUserType(event: Event) {
-    const inputElement = event.target as HTMLInputElement // Extract the target element of the event
-    this.userTyping = inputElement.value // Update userTyping with the current value of the input element
-
-    this.correctTyping = ''
+  onUserType() {
     this.mistakeText = ''
 
-    // If the current character the user has typed doesn't match the expected character...
     for (let i = 0; i < this.userTyping.length; i++) {
-      // Current character the user has typed doesn't match the expected character
       if (this.userTyping[i] !== this.textToType[i]) {
-        // ...then we append an 'Incorrect' status to our tracking string.
-        // The 'correctTyping' string acts as a tracker of the user's typing accuracy.
-        // For example, if the user types "thi" when the expected text is "the", the 'correctTyping' string will be "110",
-        // indicating the user typed the first two characters correctly and the third one incorrectly.
-        this.correctTyping += TypingStatus.Incorrect // Indicate an error has occurred
-        // Additionally, we add the incorrect character to the 'mistakeText' string.
-        // This will help in visually indicating which characters were mistyped.
         this.mistakeText += this.userTyping[i]
       } else {
-        // If the character matches the expected character, we append a 'Correct' status to our tracking string.
-        this.correctTyping += TypingStatus.Correct
-        // To ensure the 'mistakeText' visually aligns with the original text, we add a non-breaking space for correctly typed characters.
-        // This way, when overlaying the 'mistakeText' over the original text, the characters will line up correctly,
-        // and only the mistakes will be visible.
         this.mistakeText += '&nbsp;'
       }
     }
 
     this.typedText = this.textToType.slice(0, this.userTyping.length)
+    this.currentChar = this.textToType[this.userTyping.length] || ''
+    this.remainingText = this.textToType.slice(this.userTyping.length + 1)
 
-    if (!this.startTime) {
-      this.startTime = new Date().getTime()
-    }
-
-    this.currentChar = this.textToType[this.typedText.length]
-    this.remainingText = this.textToType.slice(this.typedText.length + 1)
-
-    // for switching focus to other window when finished typing in the first window
     if (this.userTyping === this.textToType) {
       this.typingCompleted.emit()
     }
+
+    if (!this.startTime) this.startTime = new Date().getTime()
 
     this.typingService.updateStats(
       this.userTyping,
@@ -154,8 +99,6 @@ export class TypingAreaComponent implements OnInit, OnChanges, AfterViewInit {
       this.startTime,
       this.language
     )
-
-    this.cd.detectChanges()
   }
 
   @HostListener('click')
@@ -165,13 +108,11 @@ export class TypingAreaComponent implements OnInit, OnChanges, AfterViewInit {
 
   onFocus() {
     this.isFocused = true
-
     this.focus.emit()
   }
 
   onBlur() {
     this.isFocused = false
-
     this.blur.emit()
   }
 }
