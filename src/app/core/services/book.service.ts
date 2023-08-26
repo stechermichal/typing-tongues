d34ae6e3c78045a85204a5631733df80c2b31580
Michal Štecher <m.stecher@cleverbee.com>
2023-08-26 16:46:56 +0200

    feat: dont make pages split in middle of sentence

--- src/app/core/services/book.service.ts
+++ src/app/core/services/book.service.ts
@@ -34,15 +34,39 @@ export class BookService {
 
   private paginateBook(rawText: string): string[] {
     let cleanedText = rawText.replace(/\n/g, ' ').replace(/\r/g, ' ')
-    cleanedText = cleanedText.replace(/  +/g, ' ') // Replace double spaces with single space
+    cleanedText = cleanedText.replace(/  +/g, ' ')
 
-    console.log('Cleaned Text: ', cleanedText.substring(4000, 6000)) // Logs first 1000 characters
-
-    const charsPerPage = 700
+    const charsPerPage = 200
     const pages = []
 
-    for (let i = 0; i < cleanedText.length; i += charsPerPage) {
-      pages.push(cleanedText.slice(i, i + charsPerPage))
+    let i = 0
+    while (i < cleanedText.length) {
+      let endIndex = i + charsPerPage
+      if (endIndex >= cleanedText.length) {
+        pages.push(cleanedText.slice(i))
+        break
+      }
+
+      // Find the last sentence-ending punctuation before endIndex
+      const sliceToCheck = cleanedText.slice(i, endIndex)
+      const lastPunctuationIndex = Math.max(
+        sliceToCheck.lastIndexOf('.'),
+        sliceToCheck.lastIndexOf('!'),
+        sliceToCheck.lastIndexOf('?')
+      )
+
+      if (lastPunctuationIndex !== -1) {
+        endIndex = i + lastPunctuationIndex + 1 // +1 to include the punctuation
+      } else {
+        // If there's no punctuation, we can break at the last space
+        const lastSpaceIndex = sliceToCheck.lastIndexOf(' ')
+        if (lastSpaceIndex !== -1) {
+          endIndex = i + lastSpaceIndex
+        }
+      }
+
+      pages.push(cleanedText.slice(i, endIndex))
+      i = endIndex
     }
 
     return pages
